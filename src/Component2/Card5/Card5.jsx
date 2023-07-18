dce77180402c1e8934e13737f5d320fcd3e399ae
Rajnish <RajnishAryan12@gmail.com>
2023-07-18 15:29:00 +0530

    card5 and footer added in partnerpage

---
+++ src/Component2/Card5/Card5.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import image14 from "../../Components/assets/Frame 52949.png"
+import image15 from "../../Components/assets/Frame 52961.png"
+import image16 from "../../Components/assets/Frame 52962.png"
+
+const Card = () => {
+  return (
+    <div className='flex justify-center gap-5 mt-5'>
+<div><img src={image14} alt="" /></div>
+<div><img src={image15} alt="" /></div>
+<div><img src={image16} alt="" /></div>
+
+
+
+    </div>
+  )
+}
+
+export default Card
--- src/Pages/Partnerpage.jsx
+++ src/Pages/Partnerpage.jsx
@@ -3,6 +3,8 @@ import Navbar from "../Components/Navbar/Navbar"
 import Card2 from '../Components/Card2/Card2'
 import Card3 from '../Components/Card3/Card3'
 import Card4 from '../Components/Card4/Card4'
+import Card5 from '../Component2/Card5/Card5'
+import Footer from "../Components/Footer/Footer"
 
 const Partnerpage = () => {
   return (
@@ -12,6 +14,8 @@ const Partnerpage = () => {
       <Card2/>
       <Card3/>
       <Card4/>
+      <Card5/>
+      <Footer/>
     </div>
   )
 }
